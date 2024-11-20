6ecb384c85fadafd63318983d265798270c45d6f
Yubin <qls773322@naver.com>
2024-11-20 19:33:04 +0900

    leetcode: Permutations

---
+++ leetcode/Permutations.py
@@ -0,0 +1,30 @@
+class Solution:
+    def permute(self, nums:list) -> list:
+        result = []
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                result.append(prev_elements[:])
+                print(prev_elements[:])
+                
+            for e in elements:
+                next_e = elements[:]
+                next_e.remove(e)
+                print(e, next_e)
+
+                prev_elements.append(e)
+                dfs(next_e)
+                prev_elements.pop()
+
+        dfs(nums)
+        return result
+    
+import itertools
+class Solution2:
+    def permute(self, nums:list) -> list:
+        print(list(itertools.permutations(nums)))
+
+nums = list(map(int, input()))
+s = Solution()
+print(s.permute(nums))
