091ae5e9e8ea3051bfd9ce6ccaa8f9082666c7ca
Yubin <qls773322@naver.com>
2024-12-07 01:21:00 +0900

    백준: 소수 구하기 - 에라토스테네스의 체

--- baekjoon/소수 구하기.py
+++ baekjoon/소수 구하기.py
@@ -13,7 +13,24 @@ class Solution:
             if isPrime:
                 print(i)
 
+    def demical_a(self):
+        M, N = map(int, input().split())
+
+        # 범위 2부터 N까지의 소수 여부를 저장할 배열
+        is_prime = [True] * (N + 1)
+        is_prime[0], is_prime[1] = False, False  # 0과 1은 소수가 아님
+
+        # 에라토스테네스의 체
+        for i in range(2, int(N**0.5) + 1):
+            if is_prime[i]:  # 소수라면
+                for j in range(i * i, N + 1, i):  # i의 배수들을 제거
+                    is_prime[j] = False
+
+        # 범위 M에서 N까지의 소수 출력
+        for i in range(M, N + 1):
+            if is_prime[i]:
+                print(i)
 
 if __name__ == "__main__":
     s = Solution()
-    s.demical_()
+    s.demical_a()
