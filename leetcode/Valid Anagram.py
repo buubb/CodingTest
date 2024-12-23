614e55ef979a530330edcee783c36f3110bf38b1
Yubin <qls773322@naver.com>
2024-12-23 18:23:13 +0900

    leetcode: Valid Anagram

---
+++ leetcode/Valid Anagram.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def isAnagram(self, s:str, t:str) -> bool:
+        return sorted(s) == sorted(t)
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isAnagram('anagram','nagaram'))
