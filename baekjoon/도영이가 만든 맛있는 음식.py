3c12bf02165744b44a9decb083ad1023b5e18f5c
Yubin <qls773322@naver.com>
2025-01-16 15:33:03 +0900

    백준: 도영이가 만든 맛있는 음식

--- baekjoon/도영이가 만든 맛있는 음식.py
+++ baekjoon/도영이가 만든 맛있는 음식.py
@@ -4,14 +4,19 @@ input = sys.stdin.readline
 class Solution:
     def doyoung_food(self):
         T = int(input())
-        s = b = 0
-        for _ in range(T):
-            S, B = map(int, input().split())
-            s = s | (1 << S-1)
-            print(s)
-            b = b | (1 << B-1)
-        print(s)
-        print(b)
+        arr = [tuple(map(int, input().split())) for _ in range(T)]
+        res = sys.maxsize
+        for i in range(1,1<<T): #1<<T는 2의 T승, 재료는 1가지 이상이므로 1부터 시작
+            bit = list(map(int, bin(i)[2:].zfill(T)))
+            total_s = 1
+            total_b = 0
+            for j in range(T):
+                s,b=arr[j]
+                if bit[j]:
+                    total_s *= s
+                    total_b += b
+            res = min(res, abs(total_s - total_b))
+        print(res)
 if __name__ == "__main__":
     s = Solution()
     s.doyoung_food()
