03fced8f103cd2bb5712143dfb769488a8fb02e6
Yubin <qls773322@naver.com>
2025-02-03 22:13:12 +0900

    review: n과 m (2)

---
+++ 백준 복습/N과 M (2).py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+class Solution:
+    def nm(self):
+        n,m=map(int, input().split())
+        arr = []
+        def dfs(k):
+            if len(arr) == m:
+                print(*arr)
+                return
+            
+            for i in range(k, n+1):
+                if i not in arr:
+                    arr.append(i)
+                    print(arr)
+                    dfs(i+1)
+                    arr.pop()
+        
+        dfs(1)
+
+if __name__ == "__main__":
+    s = Solution()
+    s.nm()
