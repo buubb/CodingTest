c2ae9888683fb9a6bdd7fa3ec792c2d1e14a388d
Yubin <qls773322@naver.com>
2025-02-04 16:40:42 +0900

    review: 카드 구매하기

--- 백준 복습/카드 구매하기.py
+++ 백준 복습/카드 구매하기.py
@@ -11,8 +11,33 @@ class Solution:
                 dp[i] = max(dp[i], dp[i-j] + cards[j])
                 print(dp)
         print(dp[n])
-       
+
+    def buy_card(self):
+        capacity = int(input())
+        arr = list(map(int, input().split()))
+        cargo = [] # (개수, 가치)
+        for i, v in enumerate(arr):
+            cargo.append((i+1, v))
+        pack = []
+        for i in range(len(cargo) + 1):
+            pack.append([])
+            for c in range(capacity+1):
+                if i == 0 or c == 0:
+                    pack[i].append(0)
+                elif cargo[i-1][0] <= c:
+                    pack[i].append(
+                        max(
+                            cargo[i-1][1] + pack[i][c-cargo[i-1][0]],
+                            pack[i-1][c]
+                        )
+                    )
+                else:
+                    pack[i].append(pack[i-1][c])
+                # print(pack)
+        print(pack[-1][-1])
+
+
 
 if __name__ == "__main__":
     s = Solution()
-    s.buy_cards()
+    s.buy_card()
