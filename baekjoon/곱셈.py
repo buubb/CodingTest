c9663e4ca09fd6262b2a063c96b1abf5a08fcbaf
Yubin <qls773322@naver.com>
2025-02-11 16:18:26 +0900

    백준: 곱셈

--- baekjoon/곱셈.py
+++ baekjoon/곱셈.py
@@ -1,2 +1,19 @@
-# a,b,c = map(int, input().split())
-# print((a**b)%c)
+import sys
+input = sys.stdin.readline
+
+class Solution:
+    def multip(self):
+        a,b,c = map(int, input().split())
+        def dac(a,b,c):
+            if b == 1:
+                return a % c
+            elif b % 2 == 0:
+                return (dac(a,b//2,c)**2) % c
+            else:
+                return (dac(a,b//2,c)**2*a) % c
+        print(dac(a,b,c))    
+        
+
+if __name__ == "__main__":
+    s = Solution()
+    s.multip()
