b96444e24eb649addeb8c49a8efde07426c04184
Yubin <qls773322@naver.com>
2025-02-13 16:27:33 +0900

    Thinking Heap

--- baekjoon/Thinking Heap.py
+++ baekjoon/Thinking Heap.py
@@ -1,60 +1,38 @@
-def thinking_heap(n, k, p):
-    # 힙 배열 초기화 (1-based index 사용)
-    heap = [None] * (n + 1)
-    insert_order = []
+import sys
+input = sys.stdin.readline
 
-    # Step 1: p 위치에 k 배치
-    heap[p] = k
-    insert_order.append(k)
-
-    # Step 2: 조상 노드 채우기
-    current_value = k - 1
-    parent = p // 2
-    while parent > 0:
-        if current_value <= 0:
+class Solution:
+    def thinking_heap(self):
+        n = int(input())
+        hp = [None] * (n+1)
+        k, p = map(int, input().split())
+        used = [False] * (n+1)
+        if k-1 < p//2:
             print(-1)
             return
-        heap[parent] = current_value
-        insert_order.append(current_value)
-        current_value -= 1
-        parent //= 2
-
-    # Step 3: 자식 노드 채우기
-    current_value = k + 1
-
-    def fill_subtree(node):
-        nonlocal current_value
-        if node > n:
-            return
-        if heap[node] is None:
-            if current_value > n:
-                print(-1)
+        
+        def btk(idx, k):
+            if idx < 0 or idx > n or k < 1 or k>n or used[k] or hp[idx]:
                 return
-            heap[node] = current_value
-            insert_order.append(current_value)
-            current_value += 1
-        fill_subtree(2 * node)
-        fill_subtree(2 * node + 1)
+            if hp[idx] is None:
+                hp[idx] = k
+                used[k] = True
+            
+            btk(idx//2, k-1)
+            btk(2*idx, k+1)
+            btk(2*idx + 1, k+2)
+        
+        btk(p, k)
+        for j in range(1,n+1):
+            for i in range(1,n+1):
+                if hp[i] is None and not used[j]:
+                    hp[i] = j
+                    break
 
-    fill_subtree(p * 2)
-    fill_subtree(p * 2 + 1)
+        for h in hp[1:]:
+            print(h)
+   
 
-    # Step 4: 나머지 노드 채우기
-    for i in range(1, n + 1):
-        if heap[i] is None:
-            while current_value in insert_order:
-                current_value += 1
-            if current_value > n:
-                print(-1)
-                return
-            heap[i] = current_value
-            insert_order.append(current_value)
-
-    # 삽입 순서 출력
-    for value in insert_order:
-        print(value)
-
-# 입력 예시
-n = int(input())
-k, p = map(int, input().split())
-thinking_heap(n, k, p)
+if __name__ == "__main__":
+    s = Solution()
+    s.thinking_heap()
