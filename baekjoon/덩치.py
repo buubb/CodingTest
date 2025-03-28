75093ba5c8fdfa6eea79772b7b90b9b4be950e2a
Yubin <qls773322@naver.com>
2025-03-28 14:23:48 +0900

    백준: 덩치

--- baekjoon/덩치.py
+++ baekjoon/덩치.py
@@ -2,21 +2,22 @@ import sys
 input = sys.stdin.readline
 
 class Solution:
-    def answer(self):
+    def answer(self): # 나 브루트 포스 왜 못하는겨
         n = int(input())
         arr = []
-        
-        for idx in range(n):
+        res = []
+        for _ in range(n):
             w, h = map(int, input().split())
-            arr.append((w+h, w, h, idx))
-       
-        arr.sort()
-        ranks = [n] * n
-        for i in range(n):
-            if arr[i][1] > arr[i+1][1] and arr[i][2] > arr[i+1][2]:
-                ranks[arr[i][3]] -= 1
+            arr.append((w, h))
             
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if arr[j][1] > arr[i][1] and arr[j][0] > arr[i][0]:
+                    count += 1
+            res.append(count+1)
 
+        print(*res)
 if __name__ == "__main__":
     s = Solution()
     s.answer()
