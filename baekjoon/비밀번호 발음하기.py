ab4a14f7b315cadc5c1df6fdd4a477e4206ed082
Yubin <qls773322@naver.com>
2025-07-10 14:48:35 +0900

    백준: 비밀번호 만들기

--- baekjoon/비밀번호 발음하기.py
+++ baekjoon/비밀번호 발음하기.py
@@ -1,9 +1,38 @@
 import sys
+from itertools import permutations
+import string
 input = sys.stdin.readline
 
 class Solution:
     def answer(self):
-        pass
+        while True:
+            str = input().rstrip()
+
+            if str == 'end':
+                return
+            
+            alpha = ['a','e','i','o','u'] # 모음
+            arr = []
+
+            for s in str:
+                if s in alpha:
+                    arr.append('0')
+                else:
+                    arr.append('1')
+            
+            isAccept = True
+            for i in range(len(str)-1):
+                if str[i] == str[i+1]:
+                    if str[i] not in ('e', 'o'):
+                        isAccept = False
+                        break
+            
+            bool_str = ''.join(arr)
+            if '0' not in bool_str or '000' in bool_str or '111' in bool_str or not isAccept:
+                print(f"<{str}> is not acceptable.")
+            else:
+                print(f"<{str}> is acceptable.")
+
 
 if __name__ == "__main__":
     s = Solution()
